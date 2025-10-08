732469463255eed7183d9c8ea8eaed504ed7a9da
Deorya <117970548+Deorya@users.noreply.github.com>
2025-10-09 03:34:59 +0800

    Add files via upload

--- basicsr/models/archs/restormer_arch.py
+++ basicsr/models/archs/restormer_arch.py
@@ -93,7 +93,7 @@ class DynamicFeedForward(nn.Module):
         )
 
         # 2. 定义多个卷积核作为 "专家"
-        kernel_sizes = [1, 3, 5]  # 示例：使用 3x3, 5x5, 7x7 卷积核
+        kernel_sizes = [3, 5, 7]  # 示例：使用 3x3, 5x5, 7x7 卷积核
         self.experts = nn.ModuleList()
         for k_size in kernel_sizes:
             padding = (k_size - 1) // 2
@@ -191,15 +191,21 @@ class Attention(nn.Module):
 
 ##########################################################################
 class TransformerBlock(nn.Module):
-    def __init__(self, dim, num_heads, ffn_expansion_factor, bias, LayerNorm_type):
+    # 我们增加一个新的参数 ffn_type，并给它一个默认值 'static'
+    def __init__(self, dim, num_heads, ffn_expansion_factor, bias, LayerNorm_type, ffn_type='static'):
         super(TransformerBlock, self).__init__()
 
         self.norm1 = LayerNorm(dim, LayerNorm_type)
         self.attn = Attention(dim, num_heads, bias)
         self.norm2 = LayerNorm(dim, LayerNorm_type)
-        # self.ffn = FeedForward(dim, ffn_expansion_factor, bias)
-        self.ffn = DynamicFeedForward(dim, ffn_expansion_factor, bias, num_experts=3)
 
+        # --- 核心修改：根据 ffn_type 参数来决定创建哪个模块 ---
+        if ffn_type == 'dynamic':
+            print("INFO: Creating DynamicFeedForward block.")
+            self.ffn = DynamicFeedForward(dim, ffn_expansion_factor, bias, num_experts=3)
+        else:  # 默认情况或 ffn_type == 'static'
+            print("INFO: Creating standard FeedForward block.")
+            self.ffn = FeedForward(dim, ffn_expansion_factor, bias)
 
     def forward(self, x):
         x = x + self.attn(self.norm1(x))
@@ -249,6 +255,7 @@ class Upsample(nn.Module):
 ##########################################################################
 ##---------- Restormer -----------------------
 class Restormer(nn.Module):
+    # 在最后增加 ffn_type 参数
     def __init__(self,
                  inp_channels=3,
                  out_channels=3,
@@ -259,7 +266,7 @@ class Restormer(nn.Module):
                  ffn_expansion_factor=2.66,
                  bias=False,
                  LayerNorm_type='WithBias',  ## Other option 'BiasFree'
-                 dual_pixel_task=False  ## True for dual-pixel defocus deblurring only. Also set inp_channels=6
+                 ffn_type='static'  # <--- 在这里添加新参数
                  ):
 
         super(Restormer, self).__init__()
@@ -268,49 +275,49 @@ class Restormer(nn.Module):
 
         self.encoder_level1 = nn.Sequential(*[
             TransformerBlock(dim=dim, num_heads=heads[0], ffn_expansion_factor=ffn_expansion_factor, bias=bias,
-                             LayerNorm_type=LayerNorm_type) for i in range(num_blocks[0])])
+                             LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_blocks[0])])
 
         self.down1_2 = Downsample(dim)  ## From Level 1 to Level 2
         self.encoder_level2 = nn.Sequential(*[
             TransformerBlock(dim=int(dim * 2 ** 1), num_heads=heads[1], ffn_expansion_factor=ffn_expansion_factor,
-                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[1])])
+                             bias=bias, LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_blocks[1])])
 
         self.down2_3 = Downsample(int(dim * 2 ** 1))  ## From Level 2 to Level 3
         self.encoder_level3 = nn.Sequential(*[
             TransformerBlock(dim=int(dim * 2 ** 2), num_heads=heads[2], ffn_expansion_factor=ffn_expansion_factor,
-                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[2])])
+                             bias=bias, LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_blocks[2])])
 
         self.down3_4 = Downsample(int(dim * 2 ** 2))  ## From Level 3 to Level 4
         self.latent = nn.Sequential(*[
             TransformerBlock(dim=int(dim * 2 ** 3), num_heads=heads[3], ffn_expansion_factor=ffn_expansion_factor,
-                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[3])])
+                             bias=bias, LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_blocks[3])])
 
         self.up4_3 = Upsample(int(dim * 2 ** 3))  ## From Level 4 to Level 3
         self.reduce_chan_level3 = nn.Conv2d(int(dim * 2 ** 3), int(dim * 2 ** 2), kernel_size=1, bias=bias)
         self.decoder_level3 = nn.Sequential(*[
             TransformerBlock(dim=int(dim * 2 ** 2), num_heads=heads[2], ffn_expansion_factor=ffn_expansion_factor,
-                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[2])])
+                             bias=bias, LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_blocks[2])])
 
         self.up3_2 = Upsample(int(dim * 2 ** 2))  ## From Level 3 to Level 2
         self.reduce_chan_level2 = nn.Conv2d(int(dim * 2 ** 2), int(dim * 2 ** 1), kernel_size=1, bias=bias)
         self.decoder_level2 = nn.Sequential(*[
             TransformerBlock(dim=int(dim * 2 ** 1), num_heads=heads[1], ffn_expansion_factor=ffn_expansion_factor,
-                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[1])])
+                             bias=bias, LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_blocks[1])])
 
         self.up2_1 = Upsample(int(dim * 2 ** 1))  ## From Level 2 to Level 1  (NO 1x1 conv to reduce channels)
 
         self.decoder_level1 = nn.Sequential(*[
             TransformerBlock(dim=int(dim * 2 ** 1), num_heads=heads[0], ffn_expansion_factor=ffn_expansion_factor,
-                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_blocks[0])])
+                             bias=bias, LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_blocks[0])])
 
         self.refinement = nn.Sequential(*[
             TransformerBlock(dim=int(dim * 2 ** 1), num_heads=heads[0], ffn_expansion_factor=ffn_expansion_factor,
-                             bias=bias, LayerNorm_type=LayerNorm_type) for i in range(num_refinement_blocks)])
+                             bias=bias, LayerNorm_type=LayerNorm_type, ffn_type=ffn_type) for i in range(num_refinement_blocks)])
 
         #### For Dual-Pixel Defocus Deblurring Task ####
-        self.dual_pixel_task = dual_pixel_task
-        if self.dual_pixel_task:
-            self.skip_conv = nn.Conv2d(dim, int(dim * 2 ** 1), kernel_size=1, bias=bias)
+        # self.dual_pixel_task = dual_pixel_task
+        # if self.dual_pixel_task:
+        #     self.skip_conv = nn.Conv2d(dim, int(dim * 2 ** 1), kernel_size=1, bias=bias)
         ###########################
 
         self.output = nn.Conv2d(int(dim * 2 ** 1), out_channels, kernel_size=3, stride=1, padding=1, bias=bias)
@@ -346,11 +353,12 @@ class Restormer(nn.Module):
         out_dec_level1 = self.refinement(out_dec_level1)
 
         #### For Dual-Pixel Defocus Deblurring Task ####
-        if self.dual_pixel_task:
-            out_dec_level1 = out_dec_level1 + self.skip_conv(inp_enc_level1)
-            out_dec_level1 = self.output(out_dec_level1)
+        # if self.dual_pixel_task:
+        #     out_dec_level1 = out_dec_level1 + self.skip_conv(inp_enc_level1)
+        #     out_dec_level1 = self.output(out_dec_level1)
         ###########################
-        else:
-            out_dec_level1 = self.output(out_dec_level1) + inp_img
+        # else:
+        #     out_dec_level1 = self.output(out_dec_level1) + inp_img
+        out_dec_level1 = self.output(out_dec_level1) + inp_img
 
         return out_dec_level1
